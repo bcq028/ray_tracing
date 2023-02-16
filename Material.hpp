86fcfa92693f026c4a9f78b9017e2624e744268b
bcq <bcq028@outlook.com>
2023-02-16 22:07:25 +0800

    add fuzzy

--- Material.hpp
+++ Material.hpp
@@ -33,15 +33,18 @@ class metal : public material {
     public:
         metal(const color& a) : albedo(a) {}
 
+        metal(const color&a,double f):albedo(a),fuzz(f<1?f:1){}
+
         virtual bool scatter(
             const Ray& r_in, const hit_record& rec, color& attenuation, Ray& scattered
         ) const override {
             vec3 reflected = reflect(normalize(r_in.direction()), rec.normal);
-            scattered = Ray(rec.p, reflected);
+            scattered = Ray(rec.p, reflected+fuzz*random_in_unit_sphere());
             attenuation = albedo;
             return (dot(scattered.direction(), rec.normal) > 0);
         }
 
     public:
         color albedo;
+        double fuzz;
 };
